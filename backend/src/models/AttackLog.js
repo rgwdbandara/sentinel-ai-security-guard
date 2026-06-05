2df9bf1dc804772df458b8b1c98ea4babaa9e95d
rgwdbandara <rgwdbandara@nsbm.students.ac.lk>
2026-06-05 09:56:42 +0530

    feat: implement mongodb attack logging system

---
+++ backend/src/models/AttackLog.js
@@ -0,0 +1,51 @@
+
+import mongoose from "mongoose";
+
+const attackLogSchema = new mongoose.Schema(
+  {
+    ip: {
+      type: String,
+      required: true,
+    },
+
+    detectedThreats: {
+      type: [String],
+      default: [],
+    },
+
+    threatScore: {
+      type: Number,
+      default: 0,
+    },
+
+    requestPath: {
+      type: String,
+    },
+
+    requestMethod: {
+      type: String,
+    },
+
+    payload: {
+      type: Object,
+      default: {},
+    },
+
+    headers: {
+      type: Object,
+      default: {},
+    },
+
+    blocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const AttackLog = mongoose.model("AttackLog", attackLogSchema);
+
+export default AttackLog;
